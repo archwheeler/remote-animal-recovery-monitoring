c78eb6a1fc91ce4653aced57b6d530a27a4e721b
Archie Wheeler <archwheeler@gmail.com>
2020-02-10 18:06:44 +0000

    Implement Chat client functionality

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,68 @@
 import React from 'react';
-//import {Card, CardHeader, CardText, CardMedia, CardActions} from 'material-ui/Card';
-//import FlatButton from 'material-ui/FlatButton';
 
 class Chat extends React.Component {
+    sendMessage(e) {
+        e.preventDefault();
+        const input = document.getElementById("message-text");
+        this.currentUser.sendSimpleMessage({
+            text: input.value,
+            roomId: this.currentUser.rooms[0].id
+        });
+        input.value = "";
+    }
+
+    componentDidMount() {
+        let chatManager = new window.Chatkit.ChatManager({
+            instanceLocator: "v1:us1:0fa440f3-996e-4157-beb1-efdc519b3973",
+            userId: "andreeahasacat",
+            tokenProvider: new window.Chatkit.TokenProvider({
+                url: "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0fa440f3-996e-4157-beb1-efdc519b3973/token"
+            })
+        });
+
+        chatManager
+        .connect()
+        .then(currentUser => { 
+            this.currentUser = currentUser;
+            this.currentUser.subscribeToRoomMultipart({
+                roomId: this.currentUser.rooms[0].id,
+                hooks: {
+                    onMessage: message => {
+                        const ul = document.getElementById("message-list");
+                        const li = document.createElement("li");
+                        li.appendChild(
+                            document.createTextNode(`${message.senderId}: ${
+                                // TODO: check the partType here.
+                                message.parts[0].payload.content
+                            }`)
+                        );
+                        ul.appendChild(li);
+                    }
+                }
+            });
+
+            this.form = document.getElementById("message-form");
+            this.form.addEventListener("submit", this.sendMessage.bind(this));
+        })
+        .catch(error => {
+            console.error("error:", error);
+        });
+            
+    }
+
+    componentWillUnmount() {
+        this.form.removeEventListener("submit", this.sendMessage.bind(this));
+        this.currentUser.disconnect();
+    }
+
 	render() {
 		return (
 			<div>
-                <p>test.</p>
+                <ul id="message-list"></ul>
+                <form id="message-form">
+                    <input type='text' id='message-text' />
+                    <input type="submit" />
+                </form>
             </div>
 		);
 	}
