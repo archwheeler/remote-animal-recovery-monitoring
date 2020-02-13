662f18476392171c532dd4e703c3e381d1fad10b
Dominic <18630143+DominicLittlewood@users.noreply.github.com>
2020-02-13 12:23:04 +0000

    Updating contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,15 +6,18 @@ class Contact extends React.Component {
 	render() {
 		return (
 			<Card>
-				<CardHeader title="Contact"/>
-				<CardMedia>
-					<img src="http://www.getmdl.io/assets/demos/welcome_card.jpg" alt=""/>
-				</CardMedia>
+				<CardHeader title="Contact Us"/>
 				<CardText>
-					This is the contact section
+					For all enquiries during office hours and for emergency referrals at any time please call us on <a href="tel:01223337621">01223 337621</a>
+					<br />
+					<br />
+					<b>Address</b>: Madingley Road, Cambridge, CB3 0ES
+
 				</CardText>
 				<CardActions>
-					<FlatButton label="Get Touch"/>
+					<FlatButton label="Telephone" href="tel:1223 337621"/>
+					<FlatButton label="Email" href="mailto:hospital@vet.cam.ac.uk"/>
+
 				</CardActions>
 			</Card>
 		);
