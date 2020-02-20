4ccb9f24810278b62f68f4366a4bbebf6da710fe
Archie Wheeler <archwheeler@gmail.com>
2020-02-20 12:18:55 +0000

    Add loading indicator to SurveyTemplate
    The loading indicator is displayed whilst the form is being
    fetched.

--- src/components/SurveyTemplate.js
+++ src/components/SurveyTemplate.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {Card, CardMedia, CardHeader, CardText} from 'material-ui/Card';
 import Avatar from 'material-ui/Avatar';
+import CircularProgress from 'material-ui/CircularProgress';
 
 class Main extends React.Component {
+	constructor() {
+		super();
+		this.state = {loading: true};
+	}
+
+	stopLoading = () => {
+		this.setState({loading: false});
+	}
+
 	render() {
 		return (
-			
 			<div>
 				<Card>
 					<CardHeader title= "George"
@@ -15,8 +24,19 @@ class Main extends React.Component {
 					<CardText>
 						Please complete the following questionnare to help monitor your dog's progress:
 					</CardText>
+					{ this.state.loading ? (
+						<div style={{display: 'flex', justifyContent: 'center'}}>
+							<CircularProgress/>
+						</div>) : null
+					}
 					<CardMedia>
-					<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe3uN1_Ew1C3pvMUUtUK1eU0vZGpslGZsqlIrOMq9ka4UjrpQ/viewform?embedded=true" width="100%" height="400" frameBorder="0">Loading…</iframe>
+					<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe3uN1_Ew1C3pvMUUtUK1eU0vZGpslGZsqlIrOMq9ka4UjrpQ/viewform?embedded=true"
+							width="100%"
+							height="400"
+							frameBorder="0"
+							onLoad={this.stopLoading}>
+								Loading…
+							</iframe>
 					</CardMedia>
 				</Card>
 			</div>
