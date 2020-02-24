4eb191bc282d976bd15eb95a0e7d582dc037af33
Archie Wheeler <archwheeler@gmail.com>
2020-02-24 14:27:29 +0000

    Fix indentation

--- src/components/ImageUploadDialog.js
+++ src/components/ImageUploadDialog.js
@@ -1,34 +1,34 @@
 import React from 'react';
-    import ImageUploader from "react-images-upload";
-    import { X } from 'react-feather';
+import ImageUploader from "react-images-upload";
+import { X } from 'react-feather';
 
-    const ImageUploadDialog = (props) => {
-      const { onDrop, closeImageUploadDialog, handleInput, fileUploadMessage, sendFile } = props;
+const ImageUploadDialog = (props) => {
+  const { onDrop, closeImageUploadDialog, handleInput, fileUploadMessage, sendFile } = props;
 
-      return (
-        <div className="dialog-container">
-          <div className="file-dialog">
-            <header>
-              <h4>Upload an image</h4>
-              <button onClick={closeImageUploadDialog} className="btn">
-                <X></X>
-              </button>
-            </header>
-            <form onSubmit={sendFile}>
-              <input placeholder="Add a message about this image" type="text" onChange={handleInput} value={fileUploadMessage} name="fileUploadMessage" />
-              <ImageUploader
-                withIcon={false}
-                buttonText="Choose images"
-                onChange={onDrop}
-                imgExtension={[".jpg", ".jpeg", ".png", ".gif"]}
-                maxFileSize={5242880}
-                withPreview={true}
-              />
-              <button type="submit" className="submit-btn">Upload</button>
-            </form>
-          </div>
-        </div>
-      )
-    }
+  return (
+    <div className="dialog-container">
+      <div className="file-dialog">
+        <header>
+          <h4>Upload an image</h4>
+          <button onClick={closeImageUploadDialog} className="btn">
+            <X></X>
+          </button>
+        </header>
+        <form onSubmit={sendFile}>
+          <input placeholder="Add a message about this image" type="text" onChange={handleInput} value={fileUploadMessage} name="fileUploadMessage" />
+          <ImageUploader
+            withIcon={false}
+            buttonText="Choose images"
+            onChange={onDrop}
+            imgExtension={[".jpg", ".jpeg", ".png", ".gif"]}
+            maxFileSize={5242880}
+            withPreview={true}
+          />
+          <button type="submit" className="submit-btn">Upload</button>
+        </form>
+      </div>
+    </div>
+  )
+}
 
-    export default ImageUploadDialog;
+export default ImageUploadDialog;
