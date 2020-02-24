7ad0c14edb49a31765e01a472214c4808e5d9e7f
Archie Wheeler <archwheeler@gmail.com>
2020-02-24 18:04:32 +0000

    Rehaul Chat system
    We are now running our own chat server.
    Next up: sending images.

--- src/chat-methods.js
+++ src/chat-methods.js
@@ -1,66 +1,87 @@
-import Chatkit from "@pusher/chatkit-client";
-import axios from "axios";
-
-function sendMessage(event) {
-  event.preventDefault();
-  const { newMessage, currentUser, currentRoom } = this.state;
-
-  if (newMessage.trim() === "") return;
-
-  const parts = [];
-  if (newMessage.trim() !== "") {
-    parts.push({
-      type: "text/plain",
-      content: newMessage
-    });
-  }
-
-  currentUser.sendMultipartMessage({
-    roomId: `${currentRoom.id}`,
-    parts
-  });
-
-  this.setState({
-    newMessage: ""
-  });
-}
+import Chatkit from '@pusher/chatkit-client';
+import axios from 'axios';
 
 function handleInput(event) {
   const { value, name } = event.target;
 
   this.setState({
-    [name]: value
+    [name]: value,
   });
 }
 
-function connectToRoom(id = currentUser.rooms[0].id) {
+function connectToChatkit() {
+
+  const { userId } = this.state;
+
+  if (userId === null || userId.trim() === '') {
+    alert('Invalid userId');
+    return;
+  }
+
+  axios
+    .post('http://localhost:5200/users', { userId })
+    .then(() => {
+      const tokenProvider = new Chatkit.TokenProvider({
+        url: 'http://localhost:5200/authenticate',
+      });
+
+      const chatManager = new Chatkit.ChatManager({
+        instanceLocator: 'v1:us1:bc55c891-bd0f-475b-8ec6-7216d20ab4cf',
+        userId,
+        tokenProvider,
+      });
+
+      return chatManager
+        .connect({
+          onAddedToRoom: room => {
+            const { rooms } = this.state;
+            this.setState({
+              rooms: [...rooms, room],
+            });
+          },
+        })
+        .then(currentUser => {
+          this.setState(
+            {
+              currentUser,
+              showLogin: false,
+              rooms: currentUser.rooms,
+            }
+          );
+          connectToRoom.call(this);
+        });
+    })
+    .catch(console.error);
+}
+
+function connectToRoom(id = '41ac18aa-8570-436c-a737-9e310afbaf3d') {
   const { currentUser } = this.state;
 
   this.setState({
-    messages: []
+    messages: [],
   });
 
   return currentUser
-    .subscribeToRoomMultipart({
+    .subscribeToRoom({
       roomId: `${id}`,
       messageLimit: 100,
       hooks: {
         onMessage: message => {
           this.setState({
-            messages: [...this.state.messages, message]
+            messages: [...this.state.messages, message],
           });
         },
         onPresenceChanged: () => {
           const { currentRoom } = this.state;
           this.setState({
             roomUsers: currentRoom.users.sort(a => {
-              if (a.presence.state === "online") return -1;
+              if (a.presence.state === 'online') return -1;
 
               return 1;
-            })
+            }),
           });
-        }
-      }
+        },
+      },
     })
     .then(currentRoom => {
       const roomName =
@@ -74,155 +95,26 @@ function connectToRoom(id = currentUser.rooms[0].id) {
         currentRoom,
         roomUsers: currentRoom.users,
         rooms: currentUser.rooms,
-        roomName
+        roomName,
       });
     })
     .catch(console.error);
 }
 
-function connectToChatkit(event) {
+function sendMessage(event) {
   event.preventDefault();
+  const { newMessage, currentUser, currentRoom } = this.state;
 
-  const { userId } = this.state;
+  if (newMessage.trim() === '') return;
 
-  if (userId === null || userId.trim() === "") {
-    alert("Invalid userId");
-    return;
-  }
-
-  this.setState({
-    isLoading: true
-  });
-
-  axios
-    .post("http://localhost:8080/users", { userId })
-    .then(() => {
-      const tokenProvider = new Chatkit.TokenProvider({
-        url: "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0fa440f3-996e-4157-beb1-efdc519b3973/token"
-      });
-
-      const chatManager = new Chatkit.ChatManager({
-        instanceLocator: "v1:us1:0fa440f3-996e-4157-beb1-efdc519b3973",
-        userId,
-        tokenProvider
-      });
-
-      return chatManager
-        .connect({
-          onAddedToRoom: room => {
-            const { rooms } = this.state;
-            this.setState({
-              rooms: [...rooms, room]
-            });
-          }
-        })
-        .then(currentUser => {
-          this.setState(
-            {
-              currentUser,
-              showLogin: false,
-              isLoading: false,
-              rooms: currentUser.rooms
-            },
-            () => connectToRoom.call(this)
-          );
-        });
-    })
-    .catch(console.error);
-}
-
-function createPrivateRoom(id) {
-  const { currentUser, rooms } = this.state;
-  const roomName = `${currentUser.id}_${id}`;
-
-  const isPrivateChatCreated = rooms.filter(room => {
-    if (room.customData && room.customData.isDirectMessage) {
-      const arr = [currentUser.id, id];
-      const { userIds } = room.customData;
-
-      if (arr.sort().join("") === userIds.sort().join("")) {
-        return {
-          room
-        };
-      }
-    }
-
-    return false;
-  });
-
-  if (isPrivateChatCreated.length > 0) {
-    return Promise.resolve(isPrivateChatCreated[0]);
-  }
-
-  return currentUser.createRoom({
-    name: `${roomName}`,
-    private: true,
-    addUserIds: [`${id}`],
-    customData: {
-      isDirectMessage: true,
-      userIds: [currentUser.id, id]
-    }
-  });
-}
-
-function onDrop(pictureFiles) {
-  this.setState({
-    pictures: this.state.pictures.concat(pictureFiles)
-  });
-}
-
-function openImageUploadDialog() {
-  this.setState({
-    showImageUploadDialog: true
-  });
-}
-
-function closeImageUploadDialog() {
-  this.setState({
-    showImageUploadDialog: false
-  });
-}
-
-function sendFile(event) {
-  event.preventDefault();
-  const { currentUser, fileUploadMessage, pictures, currentRoom } = this.state;
-
-  if (pictures.length === 0) return;
-
-  const parts = [];
-  if (fileUploadMessage.trim() !== "") {
-    parts.push({
-      type: "text/plain",
-      content: fileUploadMessage
-    });
-  }
-
-  pictures.forEach(pic => {
-    parts.push({
-      file: pic
-    });
-  });
-
-  currentUser.sendMultipartMessage({
+  currentUser.sendMessage({
+    text: newMessage,
     roomId: `${currentRoom.id}`,
-    parts
   });
 
   this.setState({
-    fileUploadMessage: "",
-    pictures: [],
-    showImageUploadDialog: false
+    newMessage: '',
   });
 }
 
-export {
-  sendMessage,
-  handleInput,
-  connectToRoom,
-  connectToChatkit,
-  sendDM,
-  onDrop,
-  sendFile,
-  openImageUploadDialog,
-  closeImageUploadDialog,
-};
+export { handleInput, connectToChatkit, connectToRoom, sendMessage }
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,142 +6,90 @@ import Avatar from 'material-ui/Avatar';
 import TextField from 'material-ui/TextField';
 import CircularProgress from 'material-ui/CircularProgress';
 
-import { Image } from "react-feather";
 import {
-    // [..]
-    onDrop,
-    openImageUploadDialog,
-    closeImageUploadDialog,
-    sendFile
-} from "../chat-methods";
-
-import ImageUploadDialog from "./ImageUploadDialog";
+    handleInput,
+    connectToChatkit,
+    connectToRoom,
+    sendMessage,
+  } from '../chat-methods';
 
 class Chat extends React.Component {
     constructor() {
         super();
         this.state = {
+            userId: 'userFoo', // TODO: get from DB
+            showLogin: true,
+            isLoading: false,
+            currentUser: null,
+            currentRoom: null,
+            rooms: [],
+            roomUsers: [],
+            roomName: null,
             messages: [],
-            pictures: [],
-            showImageUploadDialog: false,
-            fileUploadMessage: ""};
+            newMessage: ''};
 
-        this.connected = false;
+        this.handleInput = handleInput.bind(this);
+        this.connectToChatkit = connectToChatkit.bind(this);
+        this.connectToRoom = connectToRoom.bind(this);
+        this.sendMessage = sendMessage.bind(this);
 
-        this.onDrop = onDrop.bind(this);
-        this.openImageUploadDialog = openImageUploadDialog.bind(this);
-        this.closeImageUploadDialog = closeImageUploadDialog.bind(this);
-        this.sendFile = sendFile.bind(this);
-    }
-
-    sendMessage(e) {
-        e.preventDefault();
-        const input = document.getElementById("message-text");
-        this.currentUser.sendSimpleMessage({
-            text: input.value,
-            roomId: this.currentUser.rooms[0].id
-        });
-        input.value = "";
-    }
-
-    scrollToBottom = () => {
-        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
-      }
-
-    componentDidMount() {
-        let chatManager = new window.Chatkit.ChatManager({
-            instanceLocator: "v1:us1:0fa440f3-996e-4157-beb1-efdc519b3973",
-            userId: "andreeahasacat",
-            tokenProvider: new window.Chatkit.TokenProvider({
-                url: "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0fa440f3-996e-4157-beb1-efdc519b3973/token"
-            })
-        });
-
-        chatManager
-        .connect()
-        .then(currentUser => {
-            this.currentUser = currentUser;
-            this.currentUser.subscribeToRoomMultipart({
-                roomId: this.currentUser.rooms[0].id,
-                hooks: {
-                    onMessage: message => {
-                        this.setState(state => {
-                            return {messages: state.messages.concat(message)};
-                        });
-                    }
-                }
-            });
-
-            this.form = document.getElementById("message-form");
-            this.form.addEventListener("submit", this.sendMessage.bind(this));
-            this.connected = true;
-        })
-        .catch(error => {
-            console.error("error:", error);
-        });
-
-        this.scrollToBottom();
+        this.connectToChatkit();
     }
 
     componentDidUpdate() {
-        this.scrollToBottom();
-    }
-
-    componentWillUnmount() {
-        this.form.removeEventListener("submit", this.sendMessage.bind(this));
-        this.currentUser.disconnect();
-    }
+        if (this.state.currentRoom) this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    }    
 
 	render() {
         const {
-            showImageUploadDialog,
-            fileUploadMessage
-          } = this.state;
+          userId,
+          showLogin,
+          rooms,
+          currentRoom,
+          currentUser,
+          messages,
+          newMessage,
+          roomUsers,
+          roomName,
+        } = this.state;
 
 		return (
             <div>
-                {this.connected ? (
+                {this.state.currentRoom ? (
                     <Card>
                         <CardHeader title= "George"
 								subtitle="Age 13, Male, Labradoodle, QVSH Ref: 1932"
 								avatar={<Avatar>G</Avatar>}
-					    />  
+					    />
                         <List style={{maxHeight: '60vh', overflow: 'auto'}} >
-                            {this.state.messages.map(message => 
-                                <ListItem key={message.id} disabled={true}>
-                                    {message.senderId + "> " + message.parts[0].payload.content}
-                                </ListItem>)}
-                            <div style={{ float:"left", clear: "both" }}
-                                ref={(el) => { this.messagesEnd = el; }}>
-                            </div>
+                                    {this.state.messages.map(message => 
+                                        <ListItem key={message.id} disabled={true}>
+                                            {message.senderId + "> " + message.text}
+                                        </ListItem>)}
+                                    <div style={{ float:"left", clear: "both" }}
+                                        ref={(el) => { this.messagesEnd = el; }}>
+                                    </div>
                         </List>
-                    </Card>) : (
+                    </Card>
+                ) : (
                     <Card style={{padding: "15px"}}>
                         <div style={{display: 'flex', justifyContent: 'center'}}>
                             <CircularProgress/>
                         </div>
                     </Card>
-                    )}
+                )}
                 <Card style={{padding: "5px"}}>
                     
-                    <form id="message-form">
-                        <TextField id='message-text' fullWidth={true} autoComplete={"off"} hintText="Enter a message..."/>
+                    <form id="message-form" onSubmit={this.sendMessage}>
+                        <TextField id='message-text' name="newMessage" fullWidth={true} value={newMessage} autoComplete={"off"} onChange={this.handleInput} hintText="Enter a message..."/>
                     </form>
                     <button
                         onClick={this.openImageUploadDialog}
                         type="button"
                         className="btn image-picker"
-                    >Image
+                    >
+                        Attach Image
                     </button>
-                    {showImageUploadDialog ? (
-                        <ImageUploadDialog
-                            handleInput={this.handleInput}
-                            fileUploadMessage={fileUploadMessage}
-                            onDrop={this.onDrop}
-                            sendFile={this.sendFile}
-                            closeImageUploadDialog={this.closeImageUploadDialog}
-                        />
-                        ) : null}
                 </Card>
             </div>
 		);
