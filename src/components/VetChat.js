82969fc1168b13929bfe0fe617ea7db8ba59f4a8
Archie Wheeler <archwheeler@gmail.com>
2020-03-01 22:21:14 +0000

    Warn vet when they have no associated users

--- src/components/VetChat.js
+++ src/components/VetChat.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import List from 'material-ui/List/List';
-import {Card} from 'material-ui/Card';
+import {Card, CardTitle} from 'material-ui/Card';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 import CircularProgress from 'material-ui/CircularProgress';
@@ -61,55 +61,62 @@ class VetChat extends React.Component {
 
 		return (
             <div>
-                <Tabs>
-                    {this.state.rooms.map((room, index) => (
-                        <Tab 
-                        label={room.name}
-                        onActive={() => {
-                            this.setState({currentRoom: null});
-                            this.connectToRoom(room.id);
-                        }}
-                        key={room.id}>
-                            {this.state.currentRoom ? (
-                                <Card>
-                                    <List className="chat-messages" style={{maxHeight: '60vh', overflow: 'auto'}}>
-                                        <ChatSession messages={this.state.messages} />
-                                    </List>
-                                </Card>
-                            ) : (
-                                <Card style={{padding: "15px"}}>
-                                    <div style={{display: 'flex', justifyContent: 'center'}}>
-                                        <CircularProgress/>
-                                    </div>
-                                </Card>
-                            )}
-                        </Tab>
-                    ))}
-                </Tabs>
+                { (this.state.rooms.length > 0) ? (
+                    <div>
+                        <Tabs>
+                            {this.state.rooms.map((room, index) => (
+                                <Tab 
+                                label={room.name}
+                                onActive={() => {
+                                    this.setState({currentRoom: null});
+                                    this.connectToRoom(room.id);
+                                }}
+                                key={room.id}>
+                                    {this.state.currentRoom ? (
+                                        <Card>
+                                            <List className="chat-messages" style={{maxHeight: '60vh', overflow: 'auto'}}>
+                                                <ChatSession messages={this.state.messages} />
+                                            </List>
+                                        </Card>
+                                    ) : (
+                                        <Card style={{padding: "15px"}}>
+                                            <div style={{display: 'flex', justifyContent: 'center'}}>
+                                                <CircularProgress/>
+                                            </div>
+                                        </Card>
+                                    )}
+                                </Tab>
+                            ))}
+                        </Tabs>
 
-                <Card style={{padding: "5px"}}>
-                    
-                    <form id="message-form" onSubmit={this.sendMessage}>
-                        <TextField id='message-text' name="newMessage" fullWidth={true} value={newMessage} autoComplete={"off"} onChange={this.handleInput} hintText="Enter a message..."/>
-                    </form>
-                    <FlatButton
-                        onClick={this.openImageUploadDialog}
-                        type="button"
-                        className="btn image-picker"
-                        label="Attach an Image"
-                        fullWidth={true}
-                    />
-                </Card>
+                        <Card style={{padding: "5px"}}>
+                            
+                            <form id="message-form" onSubmit={this.sendMessage}>
+                                <TextField id='message-text' name="newMessage" fullWidth={true} value={newMessage} autoComplete={"off"} onChange={this.handleInput} hintText="Enter a message..."/>
+                            </form>
+                            <FlatButton
+                                onClick={this.openImageUploadDialog}
+                                type="button"
+                                className="btn image-picker"
+                                label="Attach an Image"
+                                fullWidth={true}
+                            />
+                        </Card>
 
-                {showImageUploadDialog ? (
-                    <ImageUploadDialog
-                        handleInput={this.handleInput}
-                        fileUploadMessage={fileUploadMessage}
-                        onDrop={this.onDrop}
-                        sendFile={this.sendFile}
-                        closeImageUploadDialog={this.closeImageUploadDialog}
-                    />
-                ) : null}
+                        {showImageUploadDialog ? (
+                            <ImageUploadDialog
+                                handleInput={this.handleInput}
+                                fileUploadMessage={fileUploadMessage}
+                                onDrop={this.onDrop}
+                                sendFile={this.sendFile}
+                                closeImageUploadDialog={this.closeImageUploadDialog}
+                            />
+                        ) : null}
+                    </div>) : (
+                        <Card>
+                            <CardTitle title="You are not associated with any users" />
+                        </Card>
+                    )}
             </div>
 		);
 	}
